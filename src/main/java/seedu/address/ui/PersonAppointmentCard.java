dc62e5f29618cb6417f978e73ad333f66bae36f6
Codekrodile <Codekrodile@gmail.com>
2025-04-02 14:30:04 +0800

    Update viewp fxml

--- src/main/java/seedu/address/ui/PersonAppointmentCard.java
+++ src/main/java/seedu/address/ui/PersonAppointmentCard.java
@@ -1,5 +1,6 @@
 package seedu.address.ui;
 
+import java.time.LocalDateTime;
 import java.util.Comparator;
 
 import javafx.fxml.FXML;
@@ -16,6 +17,9 @@ public class PersonAppointmentCard extends UiPart<Region> {
 
     private static final String FXML = "PersonAppointmentListCard.fxml";
 
+    public static final String PAST_APPOINTMENTS_LABEL= "Past appointments:";
+    public static final String UPCOMING_APPOINTMENTS_LABEL= "Upcoming appointments:";
+
     /**
      * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
      * As a consequence, UI elements' variable names cannot be set to such keywords
@@ -28,20 +32,28 @@ public class PersonAppointmentCard extends UiPart<Region> {
 
     @FXML
     private HBox cardPane;
+    // @FXML
+    // private Label name;
+    // @FXML
+    // private Label id;
+    // @FXML
+    // private Label phone;
+    // @FXML
+    // private Label nric;
+    // @FXML
+    // private Label dateOfBirth;
+    // @FXML
+    // private FlowPane appointments;
+    // @FXML
+    // private FlowPane tags;
     @FXML
-    private Label name;
+    private Label upcomingAppointmentsLabel;
     @FXML
-    private Label id;
+    private Label pastAppointmentsLabel;
     @FXML
-    private Label phone;
+    private FlowPane upcomingAppointments;
     @FXML
-    private Label nric;
-    @FXML
-    private Label dateOfBirth;
-    @FXML
-    private FlowPane appointments;
-    @FXML
-    private FlowPane tags;
+    private FlowPane pastAppointments;
 
     /**
      * Creates a {@code PersonCode} with the given {@code Person} and index to display.
@@ -49,16 +61,28 @@ public class PersonAppointmentCard extends UiPart<Region> {
     public PersonAppointmentCard(Person person, int displayedIndex) {
         super(FXML);
         this.person = person;
-        id.setText(displayedIndex + ". ");
-        name.setText(person.getName().fullName);
-        nric.setText(person.getNric().value);
-        phone.setText(person.getPhone().value);
-        dateOfBirth.setText(person.getDateOfBirth().toString());
+        // id.setText(displayedIndex + ". ");
+        // name.setText(person.getName().fullName);
+        // nric.setText(person.getNric().value);
+        // phone.setText(person.getPhone().value);
+        // dateOfBirth.setText(person.getDateOfBirth().toString());
+        upcomingAppointmentsLabel.setText(UPCOMING_APPOINTMENTS_LABEL);
+        pastAppointmentsLabel.setText(PAST_APPOINTMENTS_LABEL);
         person.getAppointmentList().stream()
+                .filter(appointment -> appointment.getApptDateTime().isAfter(
+                            // LocalDateTime.now()))
+                            LocalDateTime.of(2025, 5, 12, 14, 30)))
                 .sorted(Comparator.comparing(appointment -> appointment.toString()))
-                .forEach(appointment -> appointments.getChildren().add(new Label(appointment.toString())));
-        person.getTags().stream()
-                 .sorted(Comparator.comparing(tag -> tag.tagName))
-                 .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
+                .forEach(appointment -> upcomingAppointments.getChildren().add(new Label(appointment.toString())));
+        person.getAppointmentList().stream()
+                .filter(appointment -> !appointment.getApptDateTime().isAfter(
+                            // LocalDateTime.now()))
+                            LocalDateTime.of(2025, 5, 12, 14, 30)))
+                .sorted(Comparator.comparing(appointment -> appointment.toString()))
+                .forEach(appointment -> pastAppointments.getChildren().add(new Label(appointment.toString())));
+                // .forEach(appointment -> tags.getChildren().add(new Label(appointment.toString())));
+        // person.getTags().stream()
+        //          .sorted(Comparator.comparing(tag -> tag.tagName))
+        //          .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
     }
 }
