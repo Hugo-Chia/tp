519c63d5da4d45073bb181a0685d92a3099e3d20
AerollingOverDeepBlueSeas <85170877+AerollingOverDeepBlueSeas@users.noreply.github.com>
2025-04-07 20:01:03 +0800

    Add Tests for Appt Commands
    Added some tests for addappt and rmappt

--- src/test/java/seedu/address/logic/commands/AddApptCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddApptCommandTest.java
@@ -1,8 +1,11 @@
 package seedu.address.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import java.time.format.DateTimeParseException;
 
 import org.junit.jupiter.api.Test;
 
@@ -26,7 +29,7 @@ public class AddApptCommandTest {
     }
 
     @Test
-    public void addAppt_success() throws CommandException {
+    public void addFutureAppt_success() throws CommandException {
         AddApptCommand command = new AddApptCommand("S3286024I", "02/02/2026 15:00");
         CommandResult commandResult = command.execute(model);
         assertEquals(new CommandResult(String.format(AddApptCommand.MESSAGE_SUCCESS,
@@ -38,6 +41,31 @@ public class AddApptCommandTest {
         AddApptCommand command = new AddApptCommand("S3286024I", "02/02/2026 17:00");
         command.execute(model);
         assertThrows(CommandException.class, () -> command.execute(model));
+    }
+
+    @Test
+    public void addAppt_invalidPerson_fail() throws CommandException {
+        AddApptCommand command = new AddApptCommand("S1234567A", "02/02/2026 15:00");
+        assertThrows(CommandException.class, () -> command.execute(model));
+    }
+
+    @Test
+    public void addAppt_invalidDate_fail() throws DateTimeParseException {
+        AddApptCommand command = new AddApptCommand("S3286024I", "32/02/2026 15:00");
+        assertThrows(DateTimeParseException.class, () -> command.execute(model));
+    }
+
+    @Test
+    public void equals() {
+        AddApptCommand commandFirst = new AddApptCommand("S3286024I", "02/02/2026 15:00");
+        AddApptCommand commandSecond = new AddApptCommand("S3286024I", "02/02/2026 15:00");
+        AddApptCommand commandThird = new AddApptCommand("S3286024I", "02/02/2026 16:00");
+        AddApptCommand commandFourth = new AddApptCommand("S1234567A", "02/02/2026 15:00");
+
+        assertEquals(commandFirst, commandSecond);
+        assertNotEquals(commandFirst, commandThird);
+        assertNotEquals(commandFirst, commandFourth);
+
     }
 
 }
